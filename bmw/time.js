9111f3f707234cad6b732b417e025e58ec28388d
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2024-07-19 20:38:05 +0300

    mega💲

--- bmw/time.js
+++ bmw/time.js
@@ -70,7 +70,7 @@ zokou({ nomCom: 'uptime',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
 
-                 await repondre(`*_Bmw is running on a speed of: ${runtime(process.uptime())}_*`) 
+                 await repondre(`*_Bmw md uptime_: ${runtime(process.uptime())}_*`) 
 
    
 
